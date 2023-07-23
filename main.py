3db41819d977551b8ca5deb9964508620abc7743
BiP00 <benjami.pe@gmail.com>
2023-07-23 05:50:25 -0400

    Added a function to open the file, then stored the file in a variable and lowered it to be able to count all the letters but also, how many times each letters is in the book.

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello world")
+with open("books/frankenstein.txt") as f:
+    file_content = f.read()
+    lowered_file_content = file_content.lower()
+    words = lowered_file_content.split()
+    counter = 0
+    for word in words:
+        counter += 1
+    print(words)
